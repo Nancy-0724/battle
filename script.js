47bfa8fcec66227cede2af7bba2c0efd13791786
Nancy-0724 <80384305+Nancy-0724@users.noreply.github.com>
2025-08-11 01:16:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -290,6 +290,7 @@ function finishCurrentBracket(finalWinnerId){
     state.phaseLabel = "已結束";
     renderAll();
   }
+  document.getElementById("tournament").classList.add("finished");
 }
 
 /* ===== UI（最終排名＝數字 + 🥇/🥈/🥉 + 小縮圖 + 名稱） ===== */
